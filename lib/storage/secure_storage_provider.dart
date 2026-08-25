62b735129f6c4a727dc1114486e7a492de1d0c22
Do Duy Nam <duynamd33@gmail.com>
2026-08-26 00:04:26 +0700

    refactor: integrate dependency injection

--- lib/storage/secure_storage_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:food_drink_delivery/storage/secure_storage.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-part 'secure_storage_provider.g.dart';
-
-@riverpod
-SecureStorage secureStorage(Ref ref){
-  return SecureStorage();
-}
